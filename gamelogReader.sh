7be4ae90f07273cab5b53264373de98d556bfe02
DOSn3rd <dosn3rd>
2018-11-25 00:03:47 +0100

    Added urlencode to curl post

--- gamelogReader.sh
+++ gamelogReader.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 LISTEN_FILE=$(ls -r -1 $(dirname $(readlink -f ${0})) | head -n1)
-API_URL="https://example.com/api/v1337/pokeme?apikey=ubastard&username="
+API_URL="https://example.com/api/v1337/pokeme?apikey=ubastard"
 TRIGGER_STRING="PlayerLogin:"
 
 if [ "${1}" !=  "listen" ]; then
@@ -12,8 +12,8 @@ while read line; do
 
 if $( echo "${line}" | grep -q "${TRIGGER_STRING}" ); then
     PLAYER_NAME=$(echo "${line}" | cut -d":" -f4- | rev | cut -d"/" -f2- | rev | cut -c2-)
-    echo "${PLAYER_NAME}"
-#    curl -q "${API_URL}${PLAYER_NAME}"
+    echo "${PLAYER_NAME} Joined, Trigger curl"
+#   curl -q --data-urlencode "username=${PLAYER_NAME}" "${API_URL}"
 fi
 
 done
