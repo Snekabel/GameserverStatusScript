11281e3adf16f63065c84bde7aad88d299e0c522
TB <tbthegr81@gmail.com>
2018-11-24 22:48:57 +0000

    Added parsing of player name for 7Days to Die

--- gamelogReader.sh
+++ gamelogReader.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 LISTEN_FILE="./testdatastream"
-API_URL="https://example.com/api/v1337/pokeme?apikey=ubastard"
-TRIGGER_STRING=":10 "
+API_URL="https://example.com/api/v1337/pokeme?apikey=ubastard&username="
+TRIGGER_STRING="PlayerLogin:"
 
 if [ "${1}" !=  "listen" ]; then
-   tail -f "${LISTEN_FILE}" | bash ${0} listen
+   tail -n0 -f "${LISTEN_FILE}" | bash ${0} listen
 fi
 
 while read line; do
 
 if $( echo "${line}" | grep -q "${TRIGGER_STRING}" ); then
-    curl -q "${API_URL}"
+    PLAYER_NAME=$(echo "${line}" | cut -d":" -f4- | rev | cut -d"/" -f2- | rev | cut -c2-)
+    echo "${PLAYER_NAME}"
+#    curl -q "${API_URL}${PLAYER_NAME}"
 fi
 
 done
