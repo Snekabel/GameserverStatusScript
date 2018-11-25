e2cd1e4dfc6cb49416520737f82ed04e9c7be15b
TBthegr81 <tbthegr81@gmail.com>
2018-11-25 11:53:50 +0000

    Added elseif for when player disconnects

--- gamelogReader.sh
+++ gamelogReader.sh
@@ -2,9 +2,10 @@
 LISTEN_DIR="/home/steam/7days/7DaysToDieServer_Data"
 LISTEN_FILTER="output_log__*"
 LISTEN_FILE=$(ls -r -1 ${LISTEN_DIR}/${LISTEN_FILTER} | head -n1)
-REGEX="(?<=PlayerLogin: )(.*)(?=\/)";
+PLAYER_JOIN_REGEX="(?<=Player ')(.*)(?=\' joined the game)"
+PLAYER_DISCONNECT_REGEX="(?<=Player ')(.*)(?=\' left the game)"
 
-API_URL="http://example.com/api/playerJoined"
+API_URL="http://example.com/player"
 
 if [ "${1}" !=  "listen" ]; then
    tail -n0 -f "${LISTEN_FILE}" | bash ${0} listen
@@ -12,11 +13,16 @@ fi
 
 while read line; do
 
-echo "${line}"
-if $( echo "${line}" | grep -qP "${REGEX}" ); then
-    PLAYER_NAME=$(echo "${line}" | grep -oP "${REGEX}")
+#echo "${line}"
+if $( echo "${line}" | grep -qP "${PLAYER_JOIN_REGEX}" ); then
+    PLAYER_NAME=$(echo "${line}" | grep -oP "${PLAYER_JOIN_REGEX}")
     echo "${PLAYER_NAME} Joined, Trigger curl"
-    curl -q --data-urlencode "username=${PLAYER_NAME}" "${API_URL}"
+    curl -q --data-urlencode "username=${PLAYER_NAME}" "${API_URL}Joined"
+elif $( echo "${line}" | grep -qP "${PLAYER_DISCONNECT_REGEX}" ); then
+    PLAYER_NAME=$(echo "${line}" | grep -oP "${PLAYER_DISCONNECT_REGEX}")
+    echo "${PLAYER_NAME} Disconnected, Trigger curl"
+    curl -q --data-urlencode "username=${PLAYER_NAME}" "${API_URL}Disconnected"
 fi
+
 
 done
