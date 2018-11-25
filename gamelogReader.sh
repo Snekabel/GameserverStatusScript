00de70a577a45f367f4750609aa2bb32bf847665
TBthegr81 <tbthegr81@gmail.com>
2018-11-25 11:23:57 +0000

    Didn't do that right, now it works

--- gamelogReader.sh
+++ gamelogReader.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
-LISTEN_DIR="7DaysToDieServer_Data"
+LISTEN_DIR="/home/steam/7days/7DaysToDieServer_Data"
 LISTEN_FILTER="output_log__*"
-LISTEN_FILE=$(ls -r -1 "${LISTEN_DIR}/${LISTEN_FILTER}" | head -n1)
+LISTEN_FILE=$(ls -r -1 ${LISTEN_DIR}/${LISTEN_FILTER} | head -n1)
 REGEX="(?<=PlayerLogin: )(.*)(?=\/)";
 
-API_URL="http://example.com/api/"
+API_URL="http://example.com/api/playerJoined"
 
 if [ "${1}" !=  "listen" ]; then
    tail -n0 -f "${LISTEN_FILE}" | bash ${0} listen
@@ -13,8 +12,9 @@ fi
 
 while read line; do
 
-if $( echo "${line}" | grep -q "${TRIGGER_STRING}" ); then
-    PLAYER_NAME=$(echo "${line}" | grep -oP '(?<=PlayerLogin: )(.*)(?=\/)')
+echo "${line}"
+if $( echo "${line}" | grep -qP "${REGEX}" ); then
+    PLAYER_NAME=$(echo "${line}" | grep -oP "${REGEX}")
     echo "${PLAYER_NAME} Joined, Trigger curl"
     curl -q --data-urlencode "username=${PLAYER_NAME}" "${API_URL}"
 fi
