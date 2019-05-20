b7de29d8dcb6dab1f7afcafddd6c07ab5c17033f
TBthegr8 <tbthegr81@gmail.com>
2019-05-20 19:48:47 +0200

    Added example service, factorio config example and updated the gamelogreader

--- gamelogReader.sh
+++ gamelogReader.sh
@@ -16,10 +16,12 @@ while read line; do
 
 #echo "${line}"
 if $( echo "${line}" | grep -qP "${PLAYER_JOIN_FILTER}" ); then
+    echo "${line}"
     PLAYER_NAME=$(echo "${line}" | grep -oP "${PLAYER_JOIN_FILTER}")
     echo "${PLAYER_NAME} Joined, Trigger curl"
     curl -q --data-urlencode "username=${PLAYER_NAME}" --data-urlencode "game=${GAME}" "${API_URL}Joined"
 elif $( echo "${line}" | grep -qP "${PLAYER_DISCONNECT_FILTER}" ); then
+    echo "${line}"
     PLAYER_NAME=$(echo "${line}" | grep -oP "${PLAYER_DISCONNECT_FILTER}")
     echo "${PLAYER_NAME} Disconnected, Trigger curl"
     curl -q --data-urlencode "username=${PLAYER_NAME}" --data-urlencode "game=${GAME}" "${API_URL}Disconnected"
